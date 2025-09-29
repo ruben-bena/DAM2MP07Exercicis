9a65b1abdda0a2d97946d0bf8e448ff8c4165184
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2025-09-29 19:13:54 +0200

    Selecting objects with ComboBox and displaying them

--- Exercici02/src/main/java/com/project/ControllerDesktop.java
+++ Exercici02/src/main/java/com/project/ControllerDesktop.java
@@ -34,6 +34,11 @@ public class ControllerDesktop {
             comboBox.getItems().addAll("Characters", "Consoles", "Games");
             comboBox.getSelectionModel().selectFirst();
 
+            // Listener del ComboBox
+            comboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                evaluateComboBoxOption(newValue);
+            });
+
             // Obtener lista Characters
             URL jsonCharactersFileURL = getClass().getResource("/assets/characters.json");
             Path path = Paths.get(jsonCharactersFileURL.toURI());
@@ -54,6 +59,8 @@ public class ControllerDesktop {
 
             // Actualiza la UI con los valores iniciales de los personajes
             setCharacters(null);
+
+            // Añade
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -91,6 +98,105 @@ public class ControllerDesktop {
 
             // Añadir el nuevo elemento a 'yPane'
             yPane.getChildren().add(itemTemplate);
+        }
+    }
+
+    @FXML
+    private void setConsoles(ActionEvent event) throws Exception {
+        // Borrar contenido listView
+        yPane.getChildren().clear();
+
+        // Generar la nueva lista a partir de 'jsonInfoCharacters'
+        for (int i = 0; i < jsonInfoConsoles.length(); i++) {
+            // Obtenir el objeto JSON individual (character)
+            JSONObject console = jsonInfoConsoles.getJSONObject(i);
+
+            // Extraer información necesaria del JSON
+            String name = console.getString("name");
+
+            // Carregar el template de 'listItem.fxml'
+            FXMLLoader loader = new FXMLLoader(resource);
+            Parent itemTemplate = loader.load();
+            ControllerListItemDesktop itemController = loader.getController();
+
+            // Asignar los valores a los controles del template
+            itemController.setLableName(name);
+            if (name.equals("Nintendo Switch")) {
+                name = "switch";
+            } else if (name.equals("Wii U")) {
+                name = "wiiu";
+            } else if (name.equals("Nintendo 64")) {
+                name = "64";
+            } else if (name.equals("Super Nintendo")) {
+                name = "sn";
+            }
+            itemController.setImatge("/assets/images/nintendo_" + name.toLowerCase() + ".png");
+
+            // Añadir el nuevo elemento a 'yPane'
+            yPane.getChildren().add(itemTemplate);
+        }
+    }
+
+    @FXML
+    private void setGames(ActionEvent event) throws Exception {
+        // Borrar contenido listView
+        yPane.getChildren().clear();
+
+        // Generar la nueva lista a partir de 'jsonInfoCharacters'
+        for (int i = 0; i < jsonInfoGames.length(); i++) {
+            // Obtenir el objeto JSON individual (character)
+            JSONObject character = jsonInfoGames.getJSONObject(i);
+
+            // Extraer información necesaria del JSON
+            String name = character.getString("name");
+
+            // Carregar el template de 'listItem.fxml'
+            FXMLLoader loader = new FXMLLoader(resource);
+            Parent itemTemplate = loader.load();
+            ControllerListItemDesktop itemController = loader.getController();
+
+            // Asignar los valores a los controles del template
+            itemController.setLableName(name);
+            if (name.equals("The Legend of Zelda")) {
+                name = "zelda";
+            } else if (name.equals("Pokémon Red i Blue")) {
+                name = "pred";
+            } else if (name.equals("Mario Kart 64")) {
+                name = "smk";
+            } else if (name.equals("Donkey Kong")) {
+                name = "dk";
+            } else if (name.equals("Super Mario Bros")) {
+                name = "smb";
+            }
+
+            String extension = ".png";
+            if (name.equals("Metroid") || name.equals("pred") || name.equals("smk")) {
+                extension = ".jpeg";
+            }
+
+            itemController.setImatge("/assets/images/game_" + name.toLowerCase() + extension);
+
+            // Añadir el nuevo elemento a 'yPane'
+            yPane.getChildren().add(itemTemplate);
+        }
+    }
+
+    @FXML
+    private void evaluateComboBoxOption(String option) {
+        try {
+            switch (option) {
+                case "Characters":
+                    setCharacters(null);
+                    break;
+                case "Consoles":
+                    setConsoles(null);
+                    break;
+                case "Games":
+                    setGames(null);
+                    break;
+                }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }

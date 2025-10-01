e2fc025ea0dfd4d72de9f66ce7e0149465217620
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2025-10-01 18:30:15 +0200

    Desktop UI almost finished, lacking window size changes

--- Exercici02/src/main/java/com/project/ControllerDesktop.java
+++ Exercici02/src/main/java/com/project/ControllerDesktop.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -12,19 +13,26 @@ import org.json.JSONObject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.TextAlignment;
 
 public class ControllerDesktop {
 
     @FXML
-    private Text TEXTO_PRUEBA;
-    @FXML
     private ComboBox<String> comboBox;
     @FXML
-    public VBox yPane;
+    private VBox yPane;
+    @FXML
+    private Circle circle;
+    @FXML
+    private VBox mainScreen;
     private JSONArray jsonInfoCharacters, jsonInfoConsoles, jsonInfoGames;
     private URL resource = this.getClass().getResource("/assets/listItemDesktop.fxml");
 
@@ -59,9 +67,6 @@ public class ControllerDesktop {
 
             // Actualiza la UI con los valores iniciales de los personajes
             setCharacters(null);
-
-            // Añade
-
         } catch (Exception e) {
             e.printStackTrace();
 
@@ -94,10 +99,17 @@ public class ControllerDesktop {
             } else if (name.equals("Donkey Kong")) {
                 name = "dk";
             }
+
+            String urlImage = "/assets/images/character_" + name.toLowerCase() + ".png";
             itemController.setImatge("/assets/images/character_" + name.toLowerCase() + ".png");
 
             // Añadir el nuevo elemento a 'yPane'
             yPane.getChildren().add(itemTemplate);
+
+            // Añadir listener para que al hacer click se actualice la pantalla central
+            itemTemplate.setOnMouseClicked(e -> {
+                updateMainScreen(character, urlImage);
+            });
         }
     }
 
@@ -130,10 +142,17 @@ public class ControllerDesktop {
             } else if (name.equals("Super Nintendo")) {
                 name = "sn";
             }
+
+            String urlImage = "/assets/images/nintendo_" + name.toLowerCase() + ".png";
             itemController.setImatge("/assets/images/nintendo_" + name.toLowerCase() + ".png");
 
             // Añadir el nuevo elemento a 'yPane'
             yPane.getChildren().add(itemTemplate);
+
+            // Añadir listener para que al hacer click se actualice la pantalla central
+            itemTemplate.setOnMouseClicked(e -> {
+                updateMainScreen(console, urlImage);
+            });
         }
     }
 
@@ -145,10 +164,10 @@ public class ControllerDesktop {
         // Generar la nueva lista a partir de 'jsonInfoCharacters'
         for (int i = 0; i < jsonInfoGames.length(); i++) {
             // Obtenir el objeto JSON individual (character)
-            JSONObject character = jsonInfoGames.getJSONObject(i);
+            JSONObject game = jsonInfoGames.getJSONObject(i);
 
             // Extraer información necesaria del JSON
-            String name = character.getString("name");
+            String name = game.getString("name");
 
             // Carregar el template de 'listItem.fxml'
             FXMLLoader loader = new FXMLLoader(resource);
@@ -174,10 +193,16 @@ public class ControllerDesktop {
                 extension = ".jpeg";
             }
 
+            String urlImage = "/assets/images/game_" + name.toLowerCase() + extension;
             itemController.setImatge("/assets/images/game_" + name.toLowerCase() + extension);
 
             // Añadir el nuevo elemento a 'yPane'
             yPane.getChildren().add(itemTemplate);
+
+            // Añadir listener para que al hacer click se actualice la pantalla central
+            itemTemplate.setOnMouseClicked(e -> {
+                updateMainScreen(game, urlImage);
+            });        
         }
     }
 
@@ -197,6 +222,92 @@ public class ControllerDesktop {
                 }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void updateMainScreen(JSONObject jsonObject, String urlImage) {
+        // Limpiar elementos de mainScreen
+        mainScreen.getChildren().clear();
+
+        // Preparar imagen
+        try {
+            Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(urlImage)));
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(200);
+            imageView.setPreserveRatio(true);
+
+            mainScreen.getChildren().add(imageView);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        boolean isCharacter = jsonObject.has("game");
+        boolean isConsole = jsonObject.has("procesador");
+        boolean isGame = jsonObject.has("plot");
+
+        // Nombre
+        Label name = new Label(jsonObject.getString("name"));
+        name.setStyle("-fx-font-size: 30px; -fx-fill: black; -fx-font-weight: bold;");
+        mainScreen.getChildren().add(name);
+
+        // Game
+        if (isCharacter) {
+            Label game = new Label(jsonObject.getString("game"));
+            game.setStyle("-fx-font-size: 20px;");
+            mainScreen.getChildren().add(game);
+        }
+
+        // Date
+        if (isConsole) {
+            Label date = new Label(jsonObject.getString("date"));
+            date.setStyle("-fx-font-size: 20px;");
+            mainScreen.getChildren().add(date);
+        }
+
+        // Procesador
+        if (isConsole) {
+            Label procesador = new Label(jsonObject.getString("procesador"));
+            procesador.setStyle("-fx-font-size: 20px;");
+            mainScreen.getChildren().add(procesador);
+        }
+
+        // Units sold
+        if (isConsole) {
+            Label unitsSold = new Label(String.valueOf(jsonObject.getInt("units_sold")));
+            unitsSold.setStyle("-fx-font-size: 20px;");
+            mainScreen.getChildren().add(unitsSold);
+        }
+
+        // Year
+        if (isGame) {
+            Label year = new Label(String.valueOf(jsonObject.getInt("year")));
+            year.setStyle("-fx-font-size: 20px;");
+            mainScreen.getChildren().add(year);
+        }
+
+        // Type
+        if (isGame) {
+            Label type = new Label(jsonObject.getString("type"));
+            type.setStyle("-fx-font-size: 20px;");
+            mainScreen.getChildren().add(type);
+        }
+
+        // Plot
+        if (isGame) {
+            Label plot = new Label(jsonObject.getString("plot"));
+            plot.setStyle("-fx-font-size: 15px;");
+            plot.setWrapText(true);
+            plot.setAlignment(Pos.CENTER);
+            plot.setTextAlignment(TextAlignment.CENTER);
+            mainScreen.getChildren().add(plot);
+        }
+
+        // Color
+        if (isCharacter || isConsole) {
+            circle = new Circle(20);
+            circle.setStyle("-fx-fill: " + jsonObject.getString("color"));
+            mainScreen.getChildren().add(circle);
         }
     }
 }
